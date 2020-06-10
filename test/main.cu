24cab8b12ffabd60668de71a2a1aec027acc5ec4
mutsuki <mutsuki@momo86.net>
2020-06-10 22:03:55 +0900

    Update test

--- test/main.cu
+++ test/main.cu
@@ -3,8 +3,8 @@
 int main() {
 	cuda_kernel_fusing::kernel_constructor kernel_constructor(
 			"float* const dst_ptr, const float* const src_ptr",
-			"const unsigned tid",
-			"const unsigned tid = threadIdx.x;",
+			"const unsigned tid, const float& a",
+			"const unsigned tid = threadIdx.x; float a = 1.0f;",
 			""
 			);
 
@@ -13,11 +13,33 @@ int main() {
 			"device_func_0",
 			R"(
 {
-	dst_ptr[tid] = src_ptr[tid];
+	a *= src_ptr[tid];
+}
+)"
+			);
+	kernel_constructor.add_device_function(
+			"device_func_1",
+			R"(
+{
+	a /= src_ptr[tid];
+}
+)"
+			);
+	kernel_constructor.add_device_function(
+			"device_func_2",
+			R"(
+{
+	dst_ptr[tid] = a;
 }
 )"
 			);
 
 	std::printf("# -- kernel code\n");
-	std::printf("%s\n", kernel_constructor.generate_kernel_code({"device_func_0"}).c_str());
+	std::printf("%s\n", kernel_constructor.generate_kernel_code({
+				"device_func_0",
+				"device_func_1",
+				"device_func_0",
+				"device_func_1",
+				"device_func_2",
+				}).c_str());
 }
